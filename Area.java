bf85882620a5ddc1333d165828db997db81fe39c
NikithaMedikonda <160113785+NikithaMedikonda@users.noreply.github.com>
2024-02-21 11:04:23 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,6 +1,6 @@
 public class Multiplication{
         public int multiply(int a, int b,int h){
-　　　  int add=Area.addition(a,b);
+　　　   int add=Addition.addition(a,b);
         return (add/2)*h;
     }
 }
